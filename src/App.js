087e58de145414a1cf175e894f930c42c87be8b9
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-07-03 00:06:40 +0200

    dodanie NameForm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import ContactItem from "./ContactItem";
+import NameForm from "./NameForm";
 
 
 class App extends Component {
 
   render() {
     return (
+      <div>
+
       <ContactItem/>
+      <NameForm/>
+      </div>
     );
   }
 }
---
+++ src/NameForm.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: ''
+    };
+  }
+  firstNameChange(event) {
+    this.setState({firstName: event.target.value})
+  }
+  lastNameChange(event) {
+    this.setState({lastName: event.target.value})
+  }
+
+  render(){
+    return(
+      <div className='NameForm'>
+        <input type="text" value= {this.state.firstName} onInput = {this.firstNameChange.bind(this)}/>
+        <input type="text" value= {this.state.lastName} onInput = {this.lastNameChange.bind(this)} />
+        <output>{`${this.state.firstName} ${this.state.lastName}`}</output>
+      </div>
+    )
+  }
+}
+
+export default NameForm;
